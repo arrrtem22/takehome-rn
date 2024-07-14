ac695b9c87eb1fe06c97d61bef11825fbe5d45fd
Artemii Oliinyk <oliina3@medtronic.com>
2024-07-13 20:04:39 -0700

    Implement register endpoint

--- packages/back-end/src/routes/auth.ts
+++ packages/back-end/src/routes/auth.ts
@@ -1,9 +1,9 @@
 import IRoute from '../types/IRoute';
-import {Router} from 'express';
-import {compareSync} from 'bcrypt';
-import {attachSession} from '../middleware/auth';
-import {sequelize, Session, User} from '../services/db';
-import {randomBytes} from 'crypto';
+import { Router } from 'express';
+import { compareSync, hashSync } from 'bcrypt';
+import { attachSession } from '../middleware/auth';
+import { sequelize, Session, User } from '../services/db';
+import { randomBytes } from 'crypto';
 
 const AuthRouter: IRoute = {
   route: '/auth',
@@ -15,8 +15,8 @@ const AuthRouter: IRoute = {
     router.get('/', (req, res) => {
       if (req.session?.token?.id) {
         const {
-          token: {token, ...session},
-          user: {password, ...user},
+          token: { token, ...session },
+          user: { password, ...user },
         } = req.session;
         return res.json({
           success: true,
@@ -111,8 +111,88 @@ const AuthRouter: IRoute = {
     });
 
     // Attempt to register
-    router.post('/register', (req, res) => {
-      // TODO
+    router.post('/register', async (req, res) => {
+      const {
+        username,
+        password,
+      } = req.body;
+      if (!username || !password) {
+        return res.status(400).json({
+          success: false,
+          message: 'Missing username/password.',
+        });
+      }
+
+      const user = await User.findOne({
+        where: sequelize.where(
+          sequelize.fn('lower', sequelize.col('username')),
+          sequelize.fn('lower', username),
+        ),
+      }).catch(err => console.error('User lookup failed.', err));
+
+      // Ensure the user does not already exist. If they do, return an error.
+      if (user) {
+        return res.status(409).json({
+          success: false,
+          message: 'User already exists.',
+        });
+      }
+      // Hash the password
+      const hashedPassword = hashSync(password, 10);
+
+      // Create a new user
+      let newUser;
+      try {
+        newUser = await User.create({
+          username,
+          password: hashedPassword,
+        });
+      } catch (e) {
+        return passError('Failed to create user.', e, res);
+      }
+
+      if (!newUser) {
+        return passError('Returned user was nullish.', null, res);
+      }
+
+      // We now know the user is valid so it's time to mint a new session token.
+      const sessionToken = randomBytes(32).toString('hex');
+      let session;
+      try {
+        // Persist the token to the database.
+        session = await Session.create({
+          token: sessionToken,
+          user: newUser.dataValues.id,
+        });
+      } catch (e) {
+        return passError('Failed to create session.', e, res);
+      }
+
+      if (!session) {
+        // Something broke on the database side. Not much we can do.
+        return passError('Returned session was nullish.', null, res);
+      }
+
+      // We set the cookie on the response so that browser sessions will
+      // be able to use it.
+      res.cookie('SESSION_TOKEN', sessionToken, {
+        expires: new Date(Date.now() + (3600 * 24 * 7 * 1000)), // +7 days
+        secure: false,
+        httpOnly: true,
+      });
+
+      // We return the cookie to the consumer so that non-browser
+      // contexts can utilize it easily. This is a convenience for the
+      // take-home so you don't have to try and extract the cookie from
+      // the response headers etc. Just know that this is a-standard
+      // in non-oauth flows :)
+      return res.json({
+        success: true,
+        message: 'Registered Successfully.',
+        data: {
+          token: sessionToken,
+        },
+      });
     });
 
     // Log out
